8fa11a8d2c29395dcf828bcbaa91c1d63541cd43
lindabettini <linda.bettini@hotmail.it>
2022-03-01 23:22:11 +0100

    add search-bar

--- php-vue/js/script.js
+++ php-vue/js/script.js
@@ -6,6 +6,15 @@ const app = new Vue({
   el: '#root',
   data: {
     disks: [],
+    search:'',
+  },
+  methods: {
+    searchAlbum(){
+      if (!this.search) return; 
+      axios.get(`http://localhost:8889/php-ajax-dischi/api/disks?search=${this.search}`).then(res => {
+        this.disks = res.data;
+      }).catch(err => {console.log(err)})
+    }
   },
   mounted(){
     axios.get('http://localhost:8889/php-ajax-dischi/api/disks').then(res => {
